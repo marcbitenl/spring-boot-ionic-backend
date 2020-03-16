53dc4c9b6513c52668863e28aeaea876ef34bf4b
marcbitenl <marcbitenl@gmail.com>
2020-03-16 20:48:28 -0300

    Endpoint/ pedidos/{ID} disponibilizado

--- src/main/java/com/marcos/cursomc/domain/Cliente.java
+++ src/main/java/com/marcos/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.marcos.cursomc.domain.enums.TipoCliente;
 
@@ -38,9 +39,8 @@ public class Cliente implements Serializable  {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
-	
 	private List<Pedido> pedido = new ArrayList<>();
 	
 	public Cliente( ) {
