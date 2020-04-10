23b567b2d56885c901d0f9565c81b56646b55e22
marcbitenl <marcbitenl@gmail.com>
2020-04-10 16:56:55 -0300

    Adcionando senha a cliente

--- src/main/java/com/marcos/cursomc/services/DBservice.java
+++ src/main/java/com/marcos/cursomc/services/DBservice.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.marcos.cursomc.domain.Categoria;
@@ -33,6 +34,9 @@ import com.marcos.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBservice {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -112,7 +116,7 @@ public class DBservice {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "marcbitenl@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "marcbitenl@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores","300", "Apto 303", "Jardim", "38220834", cli1, c1);
