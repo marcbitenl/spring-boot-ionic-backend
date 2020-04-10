ea6c9270b2c1b8af29effccccedf0feb0674eaf4
marcbitenl <marcbitenl@gmail.com>
2020-04-10 17:28:30 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/marcos/cursomc/domain/Cliente.java
+++ src/main/java/com/marcos/cursomc/domain/Cliente.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.marcos.cursomc.domain.enums.Perfil;
 import com.marcos.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -46,11 +50,16 @@ public class Cliente implements Serializable  {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
 	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedido = new ArrayList<>();
 	
 	public Cliente( ) {
+		addPerfil(Perfil.CLIENTE);
 		
 	}
 
@@ -62,6 +71,8 @@ public class Cliente implements Serializable  {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo==null) ? null : tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
+
 	}
 
 	public Integer getId() {
@@ -110,6 +121,14 @@ public class Cliente implements Serializable  {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public Set<Perfil> getPerfil(){
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
 	}
 
 	public List<Endereco> getEnderecos() {
