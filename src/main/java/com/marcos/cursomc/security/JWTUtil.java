56ab0e6bc219e736b4014b14721e8472c4f3c80b
marcbitenl <marcbitenl@gmail.com>
2020-04-12 18:59:36 -0300

    Implementando autorização

--- src/main/java/com/marcos/cursomc/security/JWTUtil.java
+++ src/main/java/com/marcos/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;//Armazena as reinvidicações do token
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -24,4 +25,35 @@ public class JWTUtil {
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
 	}
+	
+	public boolean tokenValido (String token) {
+		Claims claims  = getClaimS(token);
+		if(claims != null ) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if(username != null  && expirationDate != null  && now.before(expirationDate))  {//Instante Atual é inferior a data de expiração
+				return true;
+			}
+		}
+	return false;
+	}
+	
+	public String getUsername (String token) {
+		Claims claims  = getClaimS(token);
+		if(claims != null ) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
+	private Claims getClaimS(String token) {// Função que recupera os claims apartir do token
+		try {
+		return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+	}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	
 }
