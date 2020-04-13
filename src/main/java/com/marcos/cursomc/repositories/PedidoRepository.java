da22207fc33a63da4545d7dfa2324a0e8560c1d7
marcbitenl <marcbitenl@gmail.com>
2020-04-12 21:38:55 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/marcos/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/marcos/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,19 @@
 package com.marcos.cursomc.repositories;
 
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.marcos.cursomc.domain.Cliente;
 import com.marcos.cursomc.domain.Pedido;
 
-@Repository 
+@Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/marcos/cursomc/services/PedidoService.java
+++ src/main/java/com/marcos/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.marcos.cursomc.domain.Cliente;
 import com.marcos.cursomc.domain.ItemPedido;
 import com.marcos.cursomc.domain.PagamentoComBoleto;
 import com.marcos.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.marcos.cursomc.domain.enums.EstadoPagamento;
 import com.marcos.cursomc.repositories.ItemPedidoRepository;
 import com.marcos.cursomc.repositories.PagamentoRepository;
 import com.marcos.cursomc.repositories.PedidoRepository;
+import com.marcos.cursomc.security.UserSS;
+import com.marcos.cursomc.services.exceptions.AuthorizationException;
 import com.marcos.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,5 +75,13 @@ public class PedidoService {
 		
 	}
 	
-	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null ) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
